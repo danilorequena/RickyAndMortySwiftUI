5fc41a2d94805e4c0eac35a1aa212d147a749b02
Danilo Requena <danilo.requena26@gmail.com>
2021-06-24 10:28:58 -0300

    Initial Commit

---
+++ RickyAndMortySwiftUI/RickyAndMortySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyAndMortySwiftUIApp.swift
+//  RickyAndMortySwiftUI
+//
+//  Created by Danilo Requena on 24/06/21.
+//
+
+import SwiftUI
+
+@main
+struct RickyAndMortySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
