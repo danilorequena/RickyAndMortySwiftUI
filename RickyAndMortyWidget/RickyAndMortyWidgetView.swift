3b15f967c09a60499e5b309a65ea6612a4b44081
Danilo Requena <danilo.requena26@gmail.com>
2021-10-13 09:22:42 -0300

    include new extension to widget

---
+++ RickyAndMortyWidget/RickyAndMortyWidgetView.swift
@@ -0,0 +1,44 @@
+//
+//  RickyAndMortyWidgetView.swift
+//  RickyAndMortySwiftUI
+//
+//  Created by Danilo Requena on 11/10/21.
+//
+
+import SwiftUI
+
+struct RickyAndMortyWidgetView: View {
+    @ObservedObject private var charactersViewModel = CharactersViewModel()
+    var body: some View {
+        HStack {
+            List {
+                ForEach(charactersViewModel.characters) { characters in
+                    NavigationLink(
+                        destination: DetailView(
+                            image: characters.image,
+                            title: characters.name,
+                            subTitle: characters.species
+                        )
+                    ) {
+                        HomeCell(
+                            title: characters.name,
+                            subTitle: characters.species,
+                            image: characters.image
+                        )
+                    }
+                }
+            }
+            .onAppear {
+                Task{
+                    await charactersViewModel.getCharactersList()
+                }
+            }
+        }
+    }
+    
+    struct RickyAndMortyWidgetView_Previews: PreviewProvider {
+        static var previews: some View {
+            RickyAndMortyWidgetView()
+        }
+    }
+}
