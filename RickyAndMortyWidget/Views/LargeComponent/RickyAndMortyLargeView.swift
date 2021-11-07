7e72bd99850aede2aa2b29252055b5d3b96fa0eb
Danilo Requena <danilo.requena26@gmail.com>
2021-11-07 10:47:43 -0300

    Add large widget

---
+++ RickyAndMortyWidget/Views/LargeComponent/RickyAndMortyLargeView.swift
@@ -0,0 +1,20 @@
+//
+//  RickyAndMortyLargeView.swift
+//  RickyAndMortySwiftUI
+//
+//  Created by Danilo Requena on 05/11/21.
+//
+
+import SwiftUI
+
+struct RickyAndMortyLargeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RickyAndMortyLargeView_Previews: PreviewProvider {
+    static var previews: some View {
+        RickyAndMortyLargeView()
+    }
+}
